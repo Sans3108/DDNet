6bf9defbe47207dd4119d0a4dc7a6d6b896daa28
Sans3108 <dragnerobertleonard13@gmail.com>
2022-09-24 18:18:45 +0300

    I forgot to rename this one

--- src/classes/Player/MapSet.ts
+++ src/classes/Player/MapSet.ts
@@ -4,8 +4,8 @@ import { MapInfo } from "./MapInfo.js";
 class MapSet {
   points: number;
   maps: MapInfo[];
-  type: Category;
-  constructor(points: number, maps: MapInfo[], type: Category) {
+  category: Category;
+  constructor(points: number, maps: MapInfo[], category: Category) {
     if (typeof points !== 'number') throw new TypeError('"points" must be of type number.');
     let mapsValid = true;
     maps.forEach(m => {
@@ -15,7 +15,7 @@ class MapSet {
 
     this.points = points;
     this.maps = maps;
-    this.type = type;
+    this.category = category;
 
     //add finished, unfinished etc
   }
